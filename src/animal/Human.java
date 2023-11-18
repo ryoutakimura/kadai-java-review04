350630208c2b8c0a13e1309a32e99ff517ddc081
ryoutakimura <yellow11053@gmail.com>
2023-11-18 23:42:10 +0900

    Humanクラス実装

---
+++ src/animal/Human.java
@@ -0,0 +1,22 @@
+package animal;
+
+public class Human extends Animal implements Thinkable{
+    public String hobby;
+    //引数なしコンストラクタ
+    public Human() {
+
+    }
+    //引数ありコンストラクタ
+    public Human(String name, int age, String hobby) {
+        super(name,age);
+        this.hobby = hobby;
+
+    }
+
+    @Override
+    public void think() {
+       System.out.println("私は" + this.hobby +"について考えています。");
+    }
+
+
+}
